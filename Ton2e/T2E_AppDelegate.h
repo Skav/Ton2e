9e3a83191255edaf6b9961d7812d12b336cb448f
Skav <senda@senda-sendas-MacBook.local>
2013-09-28 19:02:26 +0200

    Initialisation

---
+++ Ton2e/T2E_AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  T2E_AppDelegate.h
+//  Ton2e
+//
+//  Created by Skav on 9/28/13.
+//  Copyright (c) 2013 Skav. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface T2E_AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Ton2e/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Ton2e
+//
+//  Created by Skav on 9/28/13.
+//  Copyright (c) 2013 Skav. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "T2E_AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([T2E_AppDelegate class]));
+    }
+}
